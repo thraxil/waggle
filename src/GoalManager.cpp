467e25e57af467f9404dab071691b81fa39aa5c1
Anders Pearson <anders@columbia.edu>
2017-02-05 21:41:16 +0000

    make size of goals scale based on display size

--- src/GoalManager.cpp
+++ src/GoalManager.cpp
@@ -1,4 +1,5 @@
 #include "GoalManager.h"
+#define GOAL_SCALE 0.05
 
 GoalManager::GoalManager() {
 }
@@ -16,12 +17,13 @@ void GoalManager::update() {
 
 void GoalManager::draw(ofVec2f center, float radius) {
     auto step = 360.0 / goals.size();
+    auto size = GOAL_SCALE * radius;
     ofSetColor(ofColor::black);
     ofFill();
     for (int i=0; i<goals.size(); i++) {
         auto angle = ofDegToRad(i * step);
         auto x = center.x + (cos(angle) * radius);
         auto y = center.y + (sin(angle) * radius);
-        ofDrawCircle(x, y, 20);
+        ofDrawCircle(x, y, size);
     }
 }
