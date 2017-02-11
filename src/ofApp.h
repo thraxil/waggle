960b97be7069373373fdee0337853e7e19ac672d
Anders Pearson <anders@columbia.edu>
2017-02-11 10:02:52 +0000

    whitespace cleanup

--- src/ofApp.h
+++ src/ofApp.h
@@ -8,38 +8,38 @@
 
 class ofApp : public ofBaseApp{
 
-	public:
-		void setup();
-		void update();
-		void draw();
+ public:
+    void setup();
+    void update();
+    void draw();
 
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void mouseEntered(int x, int y);
+    void mouseExited(int x, int y);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
  private:
-		void drawBoundaryCircle(ofVec2f center, float radius);
-		void drawGrid(int rows, int cols, ofVec2f center, ofVec2f topLeft, float dim, int mWidth);
-		void win();
-		void lose();
-		void newGame();
-		
-		GoalManager goals;
-		MotorManager motors;
-		Sequencer sequencer;
-		SerialReader *serial_reader;
+    void drawBoundaryCircle(ofVec2f center, float radius);
+    void drawGrid(int rows, int cols, ofVec2f center, ofVec2f topLeft, float dim, int mWidth);
+    void win();
+    void lose();
+    void newGame();
 
-		ofSoundPlayer music;
-		ofSoundPlayer winSound;
-		ofSoundPlayer loseSound;
+    GoalManager goals;
+    MotorManager motors;
+    Sequencer sequencer;
+    SerialReader *serial_reader;
 
-		int goalSelected;
-		int goalTargeted;		
+    ofSoundPlayer music;
+    ofSoundPlayer winSound;
+    ofSoundPlayer loseSound;
+
+    int goalSelected;
+    int goalTargeted;
 };
